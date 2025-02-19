ff1ab97ce8ba7b7f99c2289c77d3449c57ffe5a0
fikrirazor <fikrirazor@outlook.co.id>
2025-02-19 10:16:29 +0700

    add dumy notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+    ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
