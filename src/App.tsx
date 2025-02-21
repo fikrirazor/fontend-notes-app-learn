76ab46a950d7cda39cd1f7521410472b03381420
fikrirazor <fikrirazor@outlook.co.id>
2025-02-21 11:09:02 +0700

    state handling form

--- src/App.tsx
+++ src/App.tsx
@@ -40,11 +40,26 @@ const App = () => {
       content: "bla bla note6",
     },
     ]);
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleSubmit = (
+      event: React.FormEvent
+    ) => {
+      event.preventDefault();
+      console.log("title: ", title)
+      console.log("content: ", content)
+    }
   return (
     <div className="app-container">
-      <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
+      <form 
+        className="note-form"
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <input value={title}
+        onChange={(event) => setTitle(event.target.value)} />
+        <textarea value={content} onChange={(event) => setContent(event.target.value)} />
 
         <button type="submit">Add Note</button>
       </form>
